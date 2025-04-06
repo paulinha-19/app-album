b72ce4199cd0775b9f483b10fb07fbcc1403e5fe
paulinha-19 <paulafabianasoares@gmail.com>
2025-04-06 15:29:52 -0300

    [add] hooks/index.ts

---
+++ hooks/index.ts
@@ -0,0 +1,5 @@
+import { useAuth } from "./useAuth";
+import { useTogglePassword } from "./useTooglePassword";
+import { useFocusInput } from "./useFocusInput";
+
+export {useAuth, useTogglePassword, useFocusInput}
