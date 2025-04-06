ddc32613ad9de9ba6265139cf65607ebd3fbf54b
paulinha-19 <paulafabianasoares@gmail.com>
2025-04-06 15:33:36 -0300

    [add] export const Input

---
+++ components/Input/index.ts
@@ -0,0 +1,13 @@
+import { InputRoot } from "./InputRoot";
+import { InputLabel } from "./InputLabel";
+import { InputIcon } from "./InputIcon";
+import { InputMessageError } from "./InputMessageError";
+import { InputField } from "./InputField";
+
+export const Input = {
+    Root: InputRoot,
+    Label: InputLabel,
+    Field: InputField,
+    Icon: InputIcon,
+    MessageError: InputMessageError,
+};
