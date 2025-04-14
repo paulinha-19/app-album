b3d20bb18ecc6b3c6351ab5fc483eea72a417608
paulinha-19 <paulafabianasoares@gmail.com>
2025-04-14 02:42:55 -0300

    [add] sticker detail root component

---
+++ components/StickerDetails/StickerDetailRoot.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, StyleSheet, ViewStyle } from "react-native";
+
+interface StickerDetailRootProps {
+    children: React.ReactNode;
+    style?: ViewStyle;
+}
+export function StickerDetailRoot({ children, style }: StickerDetailRootProps) {
+    return <View style={[styles.root, style]}>{children}</View>;
+}
+StickerDetailRoot.displayName = 'StickerDetail.Root'
+
+const styles = StyleSheet.create({
+    root: {
+        flex: 1,
+        alignItems: "center",
+        width: "100%",
+        height: "100%",
+        backgroundColor: "#000",
+        padding: 20,
+        paddingVertical: 30
+    }
+});
