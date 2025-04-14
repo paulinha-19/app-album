470c0cc42e148126580835b6b85881eb5c29068f
paulinha-19 <paulafabianasoares@gmail.com>
2025-04-14 02:44:25 -0300

    [add] export const Sticker composition

---
+++ components/StickerDetails/index.tsx
@@ -0,0 +1,17 @@
+import { StickerDetailRoot } from "./StickerDetailRoot"
+import { StickerDetailImage } from "./StickerDetailImage"
+import { StickerDetailTitle } from "./StickerDetailTtile"
+import { StickerDetailSubtitle } from "./StickerDetailSubtitle"
+import { StickerDetailButton } from "./StickerDetailButton"
+import { StickerDetailContent } from "./StickerDetailContent"
+import { StickerDetailFooter } from "./StickerDetailFooter"
+
+export const Sticker = {
+    Root: StickerDetailRoot,
+    Image: StickerDetailImage,
+    Title: StickerDetailTitle,
+    Subtitle: StickerDetailSubtitle,
+    Button: StickerDetailButton,
+    Content: StickerDetailContent,
+    Footer: StickerDetailFooter
+}
