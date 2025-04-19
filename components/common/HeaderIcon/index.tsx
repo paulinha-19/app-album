66d6e2dbfc1f9b8ea45c66d22e661c5288d9be73
paulinha-19 <paulafabianasoares@gmail.com>
2025-04-19 13:15:37 -0300

    [add] HeaderIcon component

---
+++ components/common/HeaderIcon/index.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { TouchableOpacity, Image, ImageSourcePropType } from "react-native";
+import { PopoverMenu } from "@/components/common";
+
+interface MenuItem {
+    label: string;
+    onPress: () => void;
+}
+
+interface FriendsMenuIconProps {
+    items: MenuItem[];
+    source?: ImageSourcePropType | undefined
+}
+
+export default function HeaderIcon({ items, source }: FriendsMenuIconProps) {
+    const [visible, setVisible] = useState(false);
+    return (
+        <>
+            <TouchableOpacity onPress={() => setVisible(true)} style={{ marginRight: 0 }}>
+                <Image
+                    source={source}
+                    style={{ width: 42, height: 42 }}
+                />
+            </TouchableOpacity>
+            <PopoverMenu
+                visible={visible}
+                onClose={() => setVisible(false)}
+                items={items}
+            />
+        </>
+    );
+}
